8347026e544a016b8bbc9c541d22f2190e48dbcd
spv <pvstrekalov@yandex.ru>
2025-06-10 19:17:48 +0400

    L40-cicd

--- L40-cicd/src/main/java/ru/otus/Main.java
+++ L40-cicd/src/main/java/ru/otus/Main.java
@@ -3,6 +3,7 @@ package ru.otus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// http://localhost:8080
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
